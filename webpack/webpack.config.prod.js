817e77ceeae81d265f5e65f92390ef3e04a2162a
kross <662242@mail.ru>
2021-10-03 21:50:20 +0500

    finall

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,53 +1,54 @@
+/* eslint-disable no-undef */
 const Webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  stats: 'errors-only',
-  bail: true,
-  output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
-    assetModuleFilename: 'assets/[name][ext]'
-  },
-  plugins: [
-    new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: './css/bundle.css',
-    }),
-  ],
+	mode: 'production',
+	devtool: 'source-map',
+	stats: 'errors-only',
+	bail: true,
+	output: {
+		filename: 'js/[name].[chunkhash:8].js',
+		chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+		assetModuleFilename: 'assets/[name][contenthash][ext]'
+	},
+	plugins: [
+		new Webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+		}),
+		new MiniCssExtractPlugin({
+			filename: './css/bundle.css',
+		}),
+	],
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.s?css/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      // {
-      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[path][name].[ext]',
-      //       },
-      //     },
-      //   ],
-      // }
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.s?css/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			// {
+			//   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+			//   use: [
+			//     {
+			//       loader: 'file-loader',
+			//       options: {
+			//         name: '[path][name].[ext]',
+			//       },
+			//     },
+			//   ],
+			// }
+		],
+	},
 });
